9f0ff71b32e28cc84c222ad1a3499e06bf4f6e91
Mac <mac@wednesday.is>
2019-09-16 20:08:11 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+echo "npm uninstall -g react-generator"
 npm uninstall -g react-generator
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
+echo "PACKAGE_VERSION: $PACKAGE_VERSION"
 PACKAGE_VERSION="$(echo  "${PACKAGE_VERSION}" | tr -d '[:space:]')"
+echo "npm install -g file:react-generator-$PACKAGE_VERSION.tgz"
 npm install -g file:react-generator-$PACKAGE_VERSION.tgz
