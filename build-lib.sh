01a3e85d08f8fe3b1b7ba36cbb37d3eef7f98772
Mac <mohammed.ali.chherawalla@gmail.com>
2020-04-12 22:40:28 +0530

    remove tarbals

--- build-lib.sh
+++ build-lib.sh
@@ -13,7 +13,8 @@ PACKAGE_VERSION=$(cat package.json \
   | awk -F: '{ print $2 }' \
   | sed 's/[",]//g')
 PACKAGE_VERSION="$(echo  "${PACKAGE_VERSION}" | tr -d '[:space:]')"
-echo "npm pack"
-npm pack
-echo "rm react-floki-$PREV_VERSION.tgz"
-rm react-floki-$PREV_VERSION.tgz
+## un comment if you want to buid a tarbal
+# echo "npm pack"
+# npm pack
+# echo "rm react-floki-$PREV_VERSION.tgz"
+# rm react-floki-$PREV_VERSION.tgz
